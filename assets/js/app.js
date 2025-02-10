b5cd4120849634b8827aa824b2b541dfb1bab87c
sachin-lakhlan <lakhlansachin0@gmail.com>
2025-02-10 15:05:38 +0530

    two section complete

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+const navbarbtn = () => {
+    document.getElementById("nav-name").classList.toggle("show-navbar")
+    document.getElementById("rotats").classList.toggle("rotate40")
+    document.getElementById("transparent").classList.toggle("bg-transparent")
+    document.getElementById("rotats2").classList.toggle("rotate-45")
+    document.body.classList.toggle("overflow-hidden")
+}
